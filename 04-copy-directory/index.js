1fdef36e0e42a7d1107f144c153e89a95e6916d5
Maria Makliuk <annamary_1995@mail.ru>
2022-05-23 23:17:20 +0300

    feat:04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,11 @@
-
+const path =require('path');
+const fs = require('fs');
+const testFolder = path.join(__dirname,'files-copy');
+//создание папки
+fs.mkdir(testFolder, ()=>{});
+// перебор файлов в папке и копирование
+fs.readdir(path.join(__dirname,'files'), (err, files) => {
+  files.forEach(file => {
+    fs.copyFile(path.join(__dirname,'files', file), path.join(__dirname,'files-copy', file), ()=>{});
+  });
+});
